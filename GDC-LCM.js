7b0edeecf8325bc23049ac4328e4a642b6d2cfe9
purumvisum <purumvisum@gmail.com>
2017-09-10 20:42:11 +0300

    Update GDC-LCM.js

--- GDC-LCM.js
+++ GDC-LCM.js
@@ -14,7 +14,66 @@ function readLine (line) {
 	let a = parseInt(line.toString().split(' ')[0])
     let b = parseInt(line.toString().split(' ')[1])
     // LCM(a, b)
-    primeFactorization( 210)
+    let simpleNumbersArrayA = primeFactorization(a);
+    let simpleNumbersArrayB = primeFactorization(b);
+
+    console.log(a, b)
+
+    let simpleNumbersAB = [];
+
+    // console.log(simpleNumbersArrayA)
+    // console.log(simpleNumbersArrayB)
+
+
+    let countsA = {}
+
+    // create object with items and them numbers 1nd array
+    simpleNumbersArrayA.forEach(function(x) { 
+        countsA[x] = (countsA[x] || 0)+1; 
+    });
+
+   let countsB = {}
+
+
+   // create object with items and them numbers 2nd array
+    simpleNumbersArrayB.forEach(function(x) { 
+        countsB[x] = (countsB[x] || 0)+1; 
+    });
+
+
+   for(var item in countsA){
+        if(countsA.hasOwnProperty(item)){
+            if(countsA[item] < countsB[item]){
+                for (let i =0; i < countsB[item]; i ++) {
+                     simpleNumbersAB.push(item)
+                }
+
+
+            } else {
+                 for (let i =0; i < countsA[item]; i ++) {
+                     simpleNumbersAB.push(item)
+                }
+            
+            }
+        }
+    }
+
+    // if in second obj more items than in first 
+    simpleNumbersArrayB.map(function(item) {
+        let includes = simpleNumbersArrayA.includes(item)
+        if (!includes) {
+            simpleNumbersAB.push(item)
+        } 
+    });
+
+    console.log('simpleNumbersAB', simpleNumbersAB)
+
+    let sum = simpleNumbersAB.reduce(function(a,b){
+        return a*b; 
+    });
+
+    console.log(sum)
+
     process.exit();
 } 
 
@@ -31,13 +90,20 @@ function GCD(a, b) {
 
 function LCM (a, b) {
     let lcm = a * b/ GCD(a, b);
-    console.log(lcm);
+    // console.log(lcm);
 }
 
 function primeFactorization (x) {
     var j = 0;
     var i = 2;
     var a = [];
+
+    if (i > x) {
+
+        a.push(x);
+         return a ;
+    }
+
     do{
         if (x % i === 0){
             a[j] = i;
@@ -52,6 +118,5 @@ function primeFactorization (x) {
         a[j] = i; 
     }
 
-    console.log(a)
+    return a ;
 }
-
